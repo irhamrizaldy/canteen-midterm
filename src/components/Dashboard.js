2f682b9ee135031830fc0abb562a4d56ec8736bb
irhamrizaldy <rirham12@gmail.com>
2020-04-01 21:59:24 +0700

    sweet alert

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,23 +4,37 @@ import { Form, Button } from 'react-bootstrap';
 export class Dashboard extends Component {
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        this.state = { fullname: '', department: '', classroom: '' };
     }
-    handleClick(e) {
-        console.log(e.currentTarget.dataset.id);
+    mySubmitHandler = (event) => {
+        event.preventDefault();
+        alert('Pesanan anda diterima: ' + "\n" +
+            "Nama: " + this.state.fullname + "\n" +
+            "Jurusan: " + this.state.department + "\n" +
+            "Kelas: " + this.state.classroom);
+    }
+
+    myChangeHandler = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({ [nam]: val });
     }
     render() {
         return (
-            <Form>
+            <Form onSubmit={this.mySubmitHandler}>
                 <Form.Group controlId="formBasicNama">
                     <Form.Label>Nama Lengkap</Form.Label>
-                    <Form.Control type="text" placeholder="Nama Mahasiswa" />
+                    <Form.Control type="text" name="fullname" placeholder="Nama Mahasiswa" onChange={this.myChangeHandler} />
                 </Form.Group>
                 <Form.Group controlId="formBasicJurusan">
                     <Form.Label>Jurusan</Form.Label>
-                    <Form.Control type="text" placeholder="Jurusan" />
+                    <Form.Control type="text" name="department" placeholder="Jurusan" onChange={this.myChangeHandler} />
                 </Form.Group>
-                <Button variant="primary" onClick={this.handleClick}>
+                <Form.Group controlId="formBasicKelas">
+                    <Form.Label>Kelas</Form.Label>
+                    <Form.Control type="text" name="classroom" placeholder="Kelas" onChange={this.myChangeHandler} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
                     Submit
                     </Button>
             </Form>
